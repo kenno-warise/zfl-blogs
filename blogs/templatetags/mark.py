6749ea1b2ec9c2f976e24bba4583adac692dc315
Kenno-Warise <wariken0523@gmail.com>
2023-08-29 11:58:08 +0900

    ユニットテストの準備

--- blogs/templatetags/mark.py
+++ blogs/templatetags/mark.py
@@ -11,8 +11,8 @@ def markdown_to_html(text):
     return mark_safe(markdownify(text))
 
 
-class EscapeHtml(Extension):
-    def extendMarkdown(self, md):
-        md.preprocessors.deregister('html_block')
-        md.inlinePatterns.deregister('html')
+# class EscapeHtml(Extension):
+#     def extendMarkdown(self, md):
+#         md.preprocessors.deregister('html_block')
+#         md.inlinePatterns.deregister('html')
 
---
+++ blogs/tests.py
@@ -0,0 +1,4 @@
+from django.test import TestCase  # type: ignore
+from django.urls import reverse  # type: ignore
+
+from blogs.templatetags.mark import markdown_to_html
